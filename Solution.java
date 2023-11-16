f4f7c629f9c85d468c4eb8d9c37017324353fa4c
luisdoan2304 <93071923+luisdoan2304@users.noreply.github.com>
2023-11-16 23:48:53 +1100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,3 +1,22 @@
+// Task description:
+// You are given an array A of N integers. You can delete some (possibly zero) of its elements. Then the remaining elements in even positions are added and elements in odd positions are subtracted:
+
+// What is the maximum possible value of S that could be obtained by performing such deletions? As the result may be large, return its last nine digits without leading zeros (in other words, return the result modulo 1,000,000,000).
+// Write a function:
+// class Solution { public int solution(int[] A);}
+// that, given an array A of length N, returns the maximum value of S (modulo 1,000,000,000).
+// Examples:
+// 1. Given], the function should return 6, because we could delete the third value in A and obtain which is the maximum possible value of S.
+// 2. Given1,5], the function should return 7, because for A = [3, 1, 5] we could obtain
+// 3. Give 1000000000,1,1000000000], the function should return 999999998 because for1000000000,1,1000000000] we could obtain S=1000000000-1+ 1000000000-1-1000000000=2999999998. That is the maximum possible value of S, but it should be returned modulo 1000000000.
+// Write an efficient algorithm for the following assumptions:
+// ·N is an integer within the range [1..200,000];
+// · Each element of array A is an integer within the range [0..1,000,000,000].
+
+
+
+
+
 package algo_solution;
 
 public class Solution {
